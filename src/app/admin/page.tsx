7131e2862b42463be5103dcb67d941471a57df8e
@ZotacD <87343757+ZotacD@users.noreply.github.com>
2026-08-15 02:30:09 +0200

    fix: Update card disposition on dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -137,7 +137,7 @@ export default async function AdminHomePage() {
         </Button>
       </header>
 
-      <div className="grid auto-rows-min gap-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+      <div className="grid auto-rows-min gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
         {cards.map((card) => {
           const Icon = card.icon;
           return (
